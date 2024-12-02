5fcdbc9dea8f637213c8fe8e0f331418668332e2
Pixyz <kaiphoenix48@gmail.com>
2024-12-02 22:40:47 +0800

    v1.4

---
+++ Assets/footer.php
@@ -0,0 +1,31 @@
+<footer>
+        <div class="site-footer">
+            <div class="stack">
+                <p class="logo-name">Shekinah Event Place Rental</p>
+                <p class="location">
+                    49 San Ignacio St, Poblacion, <br />San Jose del Monte City, Bulacan
+                </p>
+            </div>
+            <div class="site-link-group">
+                <ul class="social-links">
+                    <li>
+                        <a class="socials" href="https://www.facebook.com/shekinaheventsplace" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a class="socials" href="https://www.instagram.com/shekinaheventsplace/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <p class="text-link">
+                <a href="">Contact</a>
+                |
+                <a href="">Imprint</a>
+                |
+                <a href="">Data Privacy</a>
+            </p>
+            <p class="company-name stagger" style="--delay: 1s">Shekinah</p>
+        </div>
+        <a href='#top' class="goUp"><i class="fa-solid fa-arrow-up"></i></a>
+    </footer>
